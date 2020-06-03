5b5c01a3ed3d0ad954e2a1aa5f388def047b4b88
Stella Su <millionstars@gmail.com>
2020-06-02 22:27:19 -0700

    project 2

---
+++ Week01/TestProject2/TestProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject2
+//
+//  Created by Stella Su on 6/2/20.
+//  Copyright © 2020 Stella Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
